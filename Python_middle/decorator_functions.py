22a16a5e096e2828c8b69cfcc95bd9460e87dd41
Tekcann <ahmetsahintekcan@gmail.com>
2026-05-23 10:20:38 +0300

    Function_finish

--- Python_middle/decorator_functions.py
+++ Python_middle/decorator_functions.py
@@ -1,16 +1,62 @@
-def my_decorator(func):
-    def wrapper():
-        print("fonksiyondan önceki işlemler")
-        func()
-        print("fonksiyondan sonraki işlemler denem")
-    return wrapper()
+# fonksiyondan önce veya fonksiyondan saonra 
+# bell işlemlerin yapılmasını istediğimiz zaman bu komutları kullnaılır
+# birden fazla fonksitonda aynı işlemleri uygulayacaksak 
+# tek fonksiyonu decorator olarak kullanmak kodu daah modüler yapar
 
-@my_decorator
-def sayHello():
-    return print("hello")
+
+def my_decorator(func):# bu bir fonksiyon (funch)
+    def wrapper(name):
+        print("fonksiyondan önceki işlemler")
+        func(name)
+        print("fonksiyondan sonraki işlemler")
+    return wrapper
+
+@my_decorator # fonkiyonu gönderir
+def sayHello(name):
+    return print("hello " + name)
 
 def sayGreeting():
     print("greeting")
 
+# sayHello = my_decorator(sayHello)
+# bu kod satırı ile @my_decorator aynı işlemi yapar
+
+sayHello("Ali")
 
 
+
+import math
+import time
+
+
+def calculate_time(func):
+    def inner(*args, **kwargs):
+        start = time.time()
+        time.sleep(1)
+
+        func(*args, **kwargs)
+
+        finish = time.time()
+        print("fonksiyon " + str(finish - start) + " saniye sürdü")
+    return inner
+
+@calculate_time
+def usAlma(a, b):
+    print(math.pow(a,b))
+
+    
+@calculate_time
+def faktoriyel(num):
+    print(math.factorial(num))
+
+@calculate_time
+def toplama(a,b):
+    print(a+b)
+
+
+
+
+
+usAlma(2,3)
+faktoriyel(4)
+toplama(10, 20)
